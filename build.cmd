09617fc0d592aa21742efd9b0a44fb07be0c5528
gusev_p <gusev_p@knopka.com>
2016-06-28 19:02:44 +0500

    батник для быстрого билда

---
+++ build.cmd
@@ -0,0 +1,8 @@
+@echo off
+set dotNetBasePath=%windir%\Microsoft.NET\Framework
+if exist %dotNetBasePath%64 set dotNetBasePath=%dotNetBasePath%64
+for /R %dotNetBasePath% %%i in (*msbuild.exe) do set msbuild=%%i
+
+set target=Simple1C.sln
+
+%msbuild% /t:Rebuild /v:m /p:Configuration=Release %target% || exit /b 1
