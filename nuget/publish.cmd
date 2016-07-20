d8141d50760552aef5ce0f41bff3727fcbe44036
Egor Vladimirovich <zmeev.ev@knopka.com>
2016-07-20 17:47:12 +0500

    fix nuget

--- nuget/publish.cmd
+++ nuget/publish.cmd
@@ -3,4 +3,4 @@ set nugetPath=C:\tools\nuget.exe
 del *.nupkg
 call ..\build.cmd || exit /b -1
 call %nugetPath% pack simple1C.nuspec || exit /b -1
-%nugetPath% push *.nupkg -Source https://www.nuget.org/api/v2/package
+rem %nugetPath% push *.nupkg -Source https://www.nuget.org/api/v2/package
