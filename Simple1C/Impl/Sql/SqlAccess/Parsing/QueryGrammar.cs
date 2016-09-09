f88b45b353e9741ba0bbf90f690c590c7d57baac
gusev_p <gusev_p@knopka.com>
2016-09-09 21:58:14 +0500

    мелочи

--- Simple1C/Impl/Sql/SqlAccess/Parsing/QueryGrammar.cs
+++ Simple1C/Impl/Sql/SqlAccess/Parsing/QueryGrammar.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using Irony.Ast;
 using Irony.Parsing;
 using Simple1C.Impl.Helpers;
 using Simple1C.Impl.Sql.SqlAccess.Syntax;
@@ -14,11 +13,15 @@ namespace Simple1C.Impl.Sql.SqlAccess.Parsing
         {
             LanguageFlags = LanguageFlags.CreateAst;
 
+            //terminals
             var termComma = ToTerm(",");
             var dot = ToTerm(".");
             var termSelect = ToTerm("SELECT");
             var termFrom = ToTerm("FROM");
+            var idSimple = new IdentifierTerminal("Identifier");
+            idSimple.SetFlag(TermFlags.NoAstNode);
 
+            //nonterminals
             var selectStmt = NonTerminal("selectStmt", delegate(ParseTreeNode n)
             {
                 var elements = n.Elements();
@@ -45,15 +48,14 @@ namespace Simple1C.Impl.Sql.SqlAccess.Parsing
                 Value = n.ChildNodes.Select(x => x.Token.ValueString).JoinStrings(".")
             });
 
-            var idSimple = new IdentifierTerminal("Identifier");
-            idSimple.SetFlag(TermFlags.NoAstNode);
-
+            //rules
             selectStmt.Rule = termSelect + columnItemList + fromClauseOpt;
             columnItemList.Rule = MakePlusRule(columnItemList, termComma, columnItem);
             columnItem.Rule = id;
             fromClauseOpt.Rule = termFrom + idlist;
             idlist.Rule = MakePlusRule(idlist, termComma, id);
             id.Rule = MakePlusRule(id, dot, idSimple);
+
             Root = selectStmt;
         }
 
@@ -67,7 +69,7 @@ namespace Simple1C.Impl.Sql.SqlAccess.Parsing
 
         private static NonTerminal NonTerminal(string name, Func<ParseTreeNode, object> creator)
         {
-            return new NonTerminal(name, delegate(AstContext context, ParseTreeNode n) { n.AstNode = creator(n); });
+            return new NonTerminal(name, (context, n) => n.AstNode = creator(n));
         }
     }
 }
