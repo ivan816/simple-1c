f169fb33d332e9cde27bfed9792ea2fafbd4fc09
mskr <maxim.kropotov@gmail.com>
2016-09-21 13:36:55 +0500

    undo class1

--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using Simple1C.Impl.Sql.SchemaMapping;
-using Simple1C.Impl.Sql.SqlAccess;
-using Simple1C.Impl.Sql.Translation;
+﻿using NUnit.Framework;
 using Simple1C.Tests.Helpers;
 
 namespace Simple1C.Tests
@@ -13,20 +6,9 @@ namespace Simple1C.Tests
     internal class Class1 : TestBase
     {
         [Test]
+        [Ignore("")]
         public void Test1()
         {
-            var inputs = new[] { "C:\\Users\\mskr\\Desktop\\badQuery.1c"}
-                .Select(c=>File.ReadAllText(c, Encoding.UTF8))
-                .ToArray();
-            var translator =
-                new QueryToSqlTranslator(
-                    new PostgreeSqlSchemaStore(
-                        new PostgreeSqlDatabase("host=localhost;port=5432;Database=mskr;Username=mskr;Password=developer")),
-                    new int[0]);
-            foreach (var input in inputs)
-            {
-                Console.WriteLine(translator.Translate(input));
-            }
         }
     }
 }
