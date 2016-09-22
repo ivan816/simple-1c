1490bbaf5f49518e15ff3bdd30ce2394700bdb44
mskr <maxim.kropotov@gmail.com>
2016-09-22 20:21:16 +0500

    порвал файл с тестами. решарпер с ума сходил на большом файле

---
+++ Tests/Sql/EnumsTest.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+
+namespace Simple1C.Tests.Sql
+{
+    public class EnumsTest : TranslationTestBase
+    {
+        [Test]
+        public void EnumsNoText()
+        {
+            const string sourceSql =
+                @"select contractors.НаименованиеПолное as ContractorFullname,contractors.ЮридическоеФизическоеЛицо as ContractorType
+from справочник.Контрагенты as contractors";
+
+            const string mappings = @"Справочник.Контрагенты t1 Main
+    наименованиеполное Single f1
+    ЮридическоеФизическоеЛицо Single f2 Перечисление.ЮридическоеФизическоеЛицо
+Перечисление.ЮридическоеФизическоеЛицо t2 Main
+    ССылка Single f3
+    Порядок Single f4";
+
+            const string expectedResult = @"select
+    contractors.f1 as ContractorFullname,
+    contractors.f2 as ContractorType
+from t1 as contractors";
+
+            CheckTranslate(mappings, sourceSql, expectedResult);
+        }
+
+        [Test]
+        public void EnumsWithText()
+        {
+            const string sourceSql =
+                @"select contractors.НаименованиеПолное as ContractorFullname,ПРЕДСТАВЛЕНИЕ(contractors.ЮридическоеФизическоеЛицо) as ContractorTypeText,contractors.ЮридическоеФизическоеЛицо as ContractorType
+from справочник.Контрагенты as contractors";
+
+            const string mappings = @"Справочник.Контрагенты t1 Main
+    наименованиеполное Single f1
+    ЮридическоеФизическоеЛицо Single f2 Перечисление.ЮридическоеФизическоеЛицо
+Перечисление.ЮридическоеФизическоеЛицо t2 Main
+    ССылка Single f3
+    Порядок Single f4";
+
+            const string expectedResult =
+                @"select
+    contractors.f1 as ContractorFullname,
+    contractors.__nested_field0 as ContractorTypeText,
+    contractors.f2 as ContractorType
+from (select
+    __nested_table0.f1,
+    __nested_table2.enumValueName as __nested_field0,
+    __nested_table0.f2
+from t1 as __nested_table0
+left join t2 as __nested_table1 on __nested_table1.f3 = __nested_table0.f2
+left join simple1c__enumMappings as __nested_table2 on __nested_table2.enumName = 'ЮридическоеФизическоеЛицо' and __nested_table2.orderIndex = __nested_table1.f4) as contractors";
+
+            CheckTranslate(mappings, sourceSql, expectedResult);
+        }
+
+        [Test]
+        public void FilterByEnumValue()
+        {
+            const string sourceSql =
+                @"select contractors.НаименованиеПолное as ContractorFullname
+from справочник.Контрагенты as contractors
+where contractors.ЮридическоеФизическоеЛицо = Значение(Перечисление.ЮридическоеФизическоеЛицо.СПокупателем)";
+
+            const string mappings = @"Справочник.Контрагенты t1 Main
+    наименованиеполное Single f1
+    ЮридическоеФизическоеЛицо Single f2 Перечисление.ЮридическоеФизическоеЛицо
+Перечисление.ЮридическоеФизическоеЛицо t2 Main
+    ССылка Single f3
+    Порядок Single f4";
+
+            const string expectedResult = @"select
+    contractors.f1 as ContractorFullname
+from t1 as contractors
+where contractors.f2 = (select
+    __nested_table0.f3
+from t2 as __nested_table0
+left join simple1c__enumMappings as __nested_table1 on __nested_table1.enumName = 'ЮридическоеФизическоеЛицо' and __nested_table1.orderIndex = __nested_table0.f4
+where __nested_table1.enumValueName = 'СПокупателем')";
+
+            CheckTranslate(mappings, sourceSql, expectedResult);
+        }
+
+        [Test]
+        public void PatchGroupByWithEnumsText()
+        {
+            const string sourceSql =
+                @"select ПРЕДСТАВЛЕНИЕ(contractors.ЮридическоеФизическоеЛицо) as ContractorTypeText, count(*) as ContractorCount
+from справочник.Контрагенты as contractors
+group by contractors.ЮридическоеФизическоеЛицо";
+
+            const string mappings = @"Справочник.Контрагенты t1 Main
+    ЮридическоеФизическоеЛицо Single f2 Перечисление.ЮридическоеФизическоеЛицо
+Перечисление.ЮридическоеФизическоеЛицо t2 Main
+    ССылка Single f3
+    Порядок Single f4";
+
+            const string expectedResult =
+                @"select
+    contractors.__nested_field0 as ContractorTypeText,
+    count(*) as ContractorCount
+from (select
+    __nested_table2.enumValueName as __nested_field0
+from t1 as __nested_table0
+left join t2 as __nested_table1 on __nested_table1.f3 = __nested_table0.f2
+left join simple1c__enumMappings as __nested_table2 on __nested_table2.enumName = 'ЮридическоеФизическоеЛицо' and __nested_table2.orderIndex = __nested_table1.f4) as contractors
+group by contractors.__nested_field0";
+
+            CheckTranslate(mappings, sourceSql, expectedResult);
+        }
+    }
+}
---
+++ Tests/Sql/TranslationTestBase.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Simple1C.Impl.Helpers;
+using Simple1C.Impl.Sql.SchemaMapping;
+using Simple1C.Impl.Sql.Translation;
+using Simple1C.Tests.Helpers;
+
+namespace Simple1C.Tests.Sql
+{
+    public abstract class TranslationTestBase : TestBase
+    {
+        protected DateTime? currentDate;
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+            currentDate = null;
+        }
+
+        protected void CheckTranslate(string mappings, string sql, string expected, params int[] areas)
+        {
+            var inmemoryMappingStore = Parse(SpacesToTabs(mappings).Trim());
+            var sqlTranslator = new QueryToSqlTranslator(inmemoryMappingStore, areas)
+            {
+                CurrentDate = currentDate
+            };
+            var translated = sqlTranslator.Translate(sql);
+            var translatedLines = SpacesToTabs(translated.Trim())
+                .Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+            var expectedLines = SpacesToTabs(expected.Trim())
+                .Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+
+            Console.WriteLine("Input:\r\n{0}\r\n", sql);
+            Console.WriteLine("Translated:\r\n{0}\r\n", translated);
+            Console.WriteLine("Expected:\r\n{0}\r\n", expected);
+            Assert.That(translatedLines, Is.EqualTo(expectedLines));
+        }
+
+        private static string SpacesToTabs(string s)
+        {
+            return s.Replace("    ", "\t");
+        }
+
+        private static InMemoryMappingStore Parse(string source)
+        {
+            var tableMappings = StringHelpers.ParseLinesWithTabs(source, delegate(string s, List<string> list)
+            {
+                var tableNames = s.Split(new[] {" "}, StringSplitOptions.None);
+                return new TableMapping(tableNames[0], tableNames[1],
+                    TableMapping.ParseTableType(tableNames[2]),
+                    list.Select(PropertyMapping.Parse).ToArray());
+            });
+            return new InMemoryMappingStore(tableMappings.ToDictionary(x => x.QueryTableName,
+                StringComparer.OrdinalIgnoreCase));
+        }
+
+        private class InMemoryMappingStore : IMappingSource
+        {
+            private readonly Dictionary<string, TableMapping> mappings;
+
+            public InMemoryMappingStore(Dictionary<string, TableMapping> mappings)
+            {
+                this.mappings = mappings;
+            }
+
+            public TableMapping ResolveTable(string queryName)
+            {
+                TableMapping mapping;
+                if (mappings.TryGetValue(queryName, out mapping))
+                    return mapping;
+                throw new InvalidOperationException(string.Format("Could not find mapping by name {0}", queryName));
+            }
+        }
+    }
+}
