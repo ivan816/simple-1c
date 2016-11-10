44c9a598dc0e78b910ccfb203c2de212fd83e09e
Ivan Medvedev <medvedev.ivan816@gmail.com>
2016-11-10 18:07:57 +0500

    Удалили метод, возвращающий Reader для чтения данных из PostgreSQL. Вместо этого добавили PostgreSqlQueryTranslator, который предназначен для транслирования 1с запроса в запрос к PostgreSQL

---
+++ Simple1C/Interface/Sql/PostgreSqlQueryTranslator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Simple1C.Impl.Sql.SchemaMapping;
+using Simple1C.Impl.Sql.SqlAccess;
+using Simple1C.Impl.Sql.Translation;
+
+namespace Simple1C.Interface.Sql
+{
+    public class PostgreSqlQueryTranslator
+    {
+        private readonly PostgreeSqlSchemaStore mappingSchema;
+
+        public PostgreSqlQueryTranslator(string connectionString, int commandTimeout = 100500)
+        {
+            mappingSchema = new PostgreeSqlSchemaStore(new PostgreeSqlDatabase(connectionString, commandTimeout));
+        }
+
+        public string Transale(string query1CText)
+        {
+            return Transale(query1CText, new int[0]);
+        }
+
+        public string Transale(string query1CText, int[] areas)
+        {
+            var translator = new QueryToSqlTranslator(mappingSchema, areas)
+            {
+                CurrentDate = DateTime.Now
+            };
+            return translator.Translate(query1CText);
+        }
+    }
+}
--- Simple1C/Interface/Sql/QuerySourceInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Simple1C.Interface.Sql
-{
-    public class QuerySourceInfo
-    {
-        public string ConnectionString { get; set; }
-        public int[] Areas { get; set; }
-    }
-}
